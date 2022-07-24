65bfbb12ba7b2612432230b8c22b0ad68d7da5f3
Nikita Balenko <nikitabalenko2013@gmail.com>
2022-07-24 08:25:20 +0300

    дз к 9 семинару

---
+++ Task64/Program.cs
@@ -0,0 +1,7 @@
+﻿int Sum(int M, int N)
+{
+    if( M == N) return M;
+    else return (N + Sum(M, N - 1));
+}
+
+System.Console.WriteLine(Sum(1, 15));
---
+++ Task66/Program.cs
@@ -0,0 +1,8 @@
+﻿int Akerman(int m, int n)
+{
+    if(m == 0) return (n + 1);
+    else if(m > 0 && n == 0) return Akerman(m - 1, 1);
+    else return Akerman(m - 1, Akerman(m, n - 1));
+}
+
+System.Console.WriteLine(Akerman(2, 3));
